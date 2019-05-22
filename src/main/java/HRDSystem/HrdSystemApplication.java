8d55a5810ec566344a7bcb11ce438ad9721c0a4c
Stas Panasiuk <temnyip@gmail.com>
2019-05-22 18:56:01 +0300

    init

---
+++ src/main/java/HRDSystem/HrdSystemApplication.java
@@ -0,0 +1,13 @@
+package HRDSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrdSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HrdSystemApplication.class, args);
+  }
+
+}
